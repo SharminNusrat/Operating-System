f0b78ef3e5850692eb24cd9eb90d536a760857f4
SharminNusrat <bsse1441@iit.du.ac.bd>
2024-04-03 19:08:41 +0600

    done

---
+++ piEstimationMonteCarlo.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <math.h>
+
+int inCount;
+int totCount;
+int numThreads;
+
+
+float genRanNum();
+void* calc(void *threadId);
+
+int main (int argc, char* argv[]) {
+	
+	float pi;
+	inCount = 0;
+	totCount = 100000670;
+	numThreads = 10;
+	pthread_t threads[numThreads];
+	
+	for(long i=0; i<numThreads; i++) {
+		pthread_create(&threads[i], NULL, calc, (void *)i);
+	}
+	
+	for(int i=0; i<numThreads; i++) {
+		pthread_join(threads[i], NULL);
+	}
+	
+	pi = 4.0 * ((float)inCount / (float)totCount);
+	
+	printf("Estimation of the pi value: %f\n",pi);
+	
+	return 0;
+}
+
+float genRanNum() {
+	//srand( time(NULL) );
+	int random = rand();
+	float unit_random = random / (float) RAND_MAX;
+	
+	return unit_random;
+}
+
+void* calc(void *threadId) {
+	
+	long id = (long ) (threadId);
+	int iterations = totCount / numThreads;
+	
+	for(int i=0; i<iterations; i++) {
+		float x = genRanNum();
+		float y = genRanNum();
+		
+		float result = sqrt((x*x) + (y*y));
+		
+		if(result <=1) {
+			inCount++;
+		}
+	}
+	
+	if(id == 0) {
+		int remainingCount = totCount % numThreads;
+		for(int i=0; i<remainingCount; i++) {
+			float x = genRanNum();
+			float y = genRanNum();
+		
+			float result = sqrt((x*x) + (y*y));
+		
+			if(result <=1) {
+				inCount++;
+			}
+		}
+	}
+	
+	pthread_exit(0);
+}
